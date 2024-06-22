f5b114db6409e43362903d196854e8e45bf11f8a
Tejaswi Poornima <tejaswipoornima@gmail.com>
2024-06-22 13:27:30 +0530

    button diasable

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Pagination = ({ currentPage, totalPages, onPrevious, onNext }) => {
   return (
     <div>
-      <button onClick={onPrevious} disabled={currentPage === 1}>
+      <button onClick={onPrevious} >
         Previous
       </button>
       <span> Page {currentPage } of {totalPages} </span>
-      <button onClick={onNext} disabled={currentPage === totalPages}>
+      <button onClick={onNext} >
         Next
       </button>
     </div>
